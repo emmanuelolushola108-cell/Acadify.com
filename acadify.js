7368b5d62ae3e9d84d6ad84fa0cac67544845bec
advanceJs <emmanuelolushola108@gmail.com>
2026-04-11 10:14:34 +0100

    post section creation

--- acadify.js
+++ acadify.js
@@ -3,28 +3,28 @@ const menuIcon = document.querySelector(".hamburger-menu");
 const sideBar = document.querySelector(".side-bar");
 const overlay = document.querySelector(".side-bar-overlay");
 
-document.addEventListener("DOMContentLoaded", () => {
-  const slideCon = document.querySelector(".blog-slides");
-  const slides = document.querySelectorAll(".blog-slide");
-  const nextBtn = document.querySelector(".next-slide");
-  const prevBtn = document.querySelector(".previous-button");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const slideCon = document.querySelector(".blog-slides");
+//   const slides = document.querySelectorAll(".blog-slide");
+//   const nextBtn = document.querySelector(".next-slide");
+//   const prevBtn = document.querySelector(".previous-button");
 
-  let i = 0;
+//   let i = 0;
 
-  nextBtn.addEventListener("click", () => {
-    if (i < slides.length - 1) {
-      i++;
-      slideCon.style.transform = `translateX(-${i * 100}%)`;
-    }
-  });
-  prevBtn.addEventListener("click", () => {
-    if (i > 0) {
-      i--;
-      console.log(i);
-      slideCon.style.transform = `translateX(-${i * 100}%)`;
-    }
-  });
-});
+//   nextBtn.addEventListener("click", () => {
+//     if (i < slides.length - 1) {
+//       i++;
+//       slideCon.style.transform = `translateX(-${i * 100}%)`;
+//     }
+//   });
+//   prevBtn.addEventListener("click", () => {
+//     if (i > 0) {
+//       i--;
+//       console.log(i);
+//       slideCon.style.transform = `translateX(-${i * 100}%)`;
+//     }
+//   });
+// });
 
 menuIcon.addEventListener("click", () => {
   sideBar.classList.add("active");
